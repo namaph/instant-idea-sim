4965ec2c68728d10b2e7717b643519c6b478d59d
takesei <sei06k14@gmail.com>
2022-03-07 08:27:23 +0900

    :wrench: Allow localhost access (cors)

--- instant_sim/app.py
+++ instant_sim/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from . import app_model, app_viz
 from .service import types as T
@@ -24,6 +25,13 @@ app = FastAPI()
 app.include_router(app_model.router)
 app.include_router(app_viz.router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,   # 追記により追加
+    allow_methods=["*"],      # 追記により追加
+    allow_headers=["*"]       # 追記により追加
+)
 
 @app.get("/", response_model=T.resp.Home)
 def read_root():
